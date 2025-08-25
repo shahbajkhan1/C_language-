4651e1906fd5dd97a6cb8ba022712952b17f5b1d
shahbajkhan1 <skshahbaj01@gmail.com>
2025-08-25 16:25:55 +0530

    upload my file

---
+++ main.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+int main()
+{
+	/*   int a = 45;
+	   int b = 7;
+	   int c  = a + b;
+
+	   printf("%d",c);*/
+
+	/*int d= 225;
+	 int f = 55;
+	 int g = d - f;
+
+	 printf("%d",g);
+	  */
+
+	/* int s = 2;
+	 int p = 2;
+	 int q = s * p;
+	 printf("%d",q);*/
+
+	/*int e = 2;
+	int z = 2;
+	int l = e / z;
+	printf("%d",l);*/
+
+	/* int m = 34;
+	 int x = 67;
+	 int a = 23;
+	 int w = 12;
+	 int c = 6;
+	 int sumValue = m+x+a+w+c;
+	 printf("%d",sumValue);*/
+
+
+	/* int a = 56;
+	 int b =34;
+	 int c = 12;
+	 int d = 2;
+	 int e = 13;
+	 int cal = a*b+c/d-e;
+	 printf("%d",cal);*/
+
+	/* 1.6*(12-2)/2+7.8-1.5*/
+
+	/* float a = 1.6;
+	 int b = 12;
+	 int c = 2;
+	 int d = 2;
+	 float e = 7.8;
+	 float f = 1.5;
+	 float calValue = a*(b-c)/d+e-f;
+
+	 printf("%f",calValue);*/
+
+
+
+
+}
